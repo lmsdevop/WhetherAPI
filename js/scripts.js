6a7f7bf942bd94d8615488cfe642c51a47083b0b
Lucas Mendonça <lms.devop@gmail.com>
2024-02-19 13:26:35 -0300

    Reformule to BYU Idaho Project

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,13 @@ const windElement = document.querySelector("#wind span")
 
 const weatherContainer = document.querySelector("#weather-data")
 
-// Funções
-const getWeatherData =  async (city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-        const res = await fetch(apiWeatherURL);
-        const data = await res.json();
+// Functions
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=eng`;
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
 
-        return data 
+    return data
 };
 const showWeatherData = async (city) => {
     const data = await getWeatherData(city);
@@ -35,7 +35,7 @@ const showWeatherData = async (city) => {
     windElement.innerText = `${data.wind.speed}km/h`;
     countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/32.png`);
 
-    weatherContainer.classList.remove("hide") 
+    weatherContainer.classList.remove("hide")
 };
 
 // Eventos
@@ -49,7 +49,7 @@ searchBtn.addEventListener("click", (e) => {
 
 cityInput.addEventListener("keyup", (e) => {
 
-    if(e.code === "Enter") {
+    if (e.code === "Enter") {
         const city = e.target.value;
 
         showWeatherData(city);
